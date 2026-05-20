b59c120c5926ba05ff769eeb83be1e6c2a09a4fd
Vighnesh-Patidar <vighneshp22@iitk.ac.in>
2026-05-20 10:42:08 -0400

    feat: filter engine implementation

---
+++ include/filter_engine.hpp
@@ -0,0 +1,32 @@
+#ifndef FILTER_ENGINE_H
+#define FILTER_ENGINE_H
+
+#include "c_types.h"
+#include "frame_parser.hpp"
+
+constexpr uint8_t MAX_RULES = 16;
+enum class Action: uint8_t{LOG, COUNT, IGNORE};
+
+struct Rule
+{
+    uint32_t src_ip_mask;
+    uint32_t src_ip_match;
+    uint16_t dst_port;
+    uint8_t protocol;
+    Action action;
+};
+
+
+class FilterEngine
+{
+    static Rule rules[MAX_RULES];
+    static uint8_t rule_count;
+public:
+    static void apply(const ParsedFrame* frame);
+    static uint32_t unmatched_count;
+    static bool add_rule(Rule rule);
+
+};
+
+
+#endif
---
+++ src/filter_engine.cpp
@@ -0,0 +1,49 @@
+#include "filter_engine.hpp"
+
+Rule FilterEngine::rules[MAX_RULES];
+uint8_t FilterEngine::rule_count = 0;
+uint32_t FilterEngine::unmatched_count = 0;
+
+bool FilterEngine::add_rule(Rule rule)
+{
+    if(rule_count >= MAX_RULES)
+    {
+        return false;
+    } else {
+        rules[rule_count++] = rule;
+    }
+    return true;
+}
+
+void FilterEngine::apply(const ParsedFrame* frame)
+{
+    if(frame == nullptr)
+    {
+        return ;
+    }
+    for(uint8_t i = 0; i < rule_count; i++)
+    {
+        const Rule& rule = rules[i];
+        bool proto_match= (rule.protocol == 0 || rule.protocol == frame->protocol);
+        bool port_match  = (rule.dst_port == 0 || rule.dst_port == frame->dst_port);
+        bool ip_match    = ((frame->src_ip & rule.src_ip_mask) == rule.src_ip_match);
+        if (proto_match && port_match && ip_match)
+        {
+            switch (rule.action)
+            {
+                case Action::LOG:
+                    os_printf("MATCH: src=%08x dst_port=%d proto=%d\n",frame->src_ip, frame->dst_port, frame->protocol);
+                    break;
+                case Action::COUNT:
+                    unmatched_count++;  
+                    break;
+                case Action::IGNORE:
+                    return;
+            }
+            return; 
+        }
+    }
+    unmatched_count++;
+    os_printf("WARN: unmatched frame src=%08x proto=%d\n",
+    frame->src_ip, frame->protocol);
+}

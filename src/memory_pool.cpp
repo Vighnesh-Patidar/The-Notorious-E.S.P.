517228e77e1bc520d2c61405d6964f858e25da4f
Vighnesh-Patidar <vighneshp22@iitk.ac.in>
2026-05-20 02:38:24 -0400

    feat: memory pool slab allocator

---
+++ src/memory_pool.cpp
@@ -0,0 +1,27 @@
+#include "memory_pool.hpp"
+
+bool MemoryPool::bitmap[POOL_DEPTH];
+uint8_t MemoryPool::pool_array[POOL_DEPTH][FRAME_SLOT_SIZE];
+
+
+uint8_t* MemoryPool::acquire()
+{
+    for(int i = 0; i < POOL_DEPTH; i++)
+    {
+        if(!bitmap[i])
+        {
+            bitmap[i] = true;
+            return pool_array[i];
+        }
+    }
+    return nullptr;
+}
+
+void MemoryPool::release(uint8_t* slot)
+{
+    int offset = slot - pool_array[0];
+    int index = offset / FRAME_SLOT_SIZE;
+    if(index > POOL_DEPTH) return;
+    bitmap[index] = false;
+
+}
